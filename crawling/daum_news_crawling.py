3dd01213534a4dd39201638396b14201df21295c
zgotter <shkim4738@gmail.com>
2021-12-14 12:55:42 +0900

    feat: 크롤링 argument 추가 및 multiprocessing 적용

--- crawling/daum_news_crawling.py
+++ crawling/daum_news_crawling.py
@@ -1,3 +1,5 @@
+import argparse
+from datetime import date, timedelta
 import json
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -22,6 +24,7 @@ class CrawlingDaumNews:
     
     def __init__(self):
         self.driver = self._get_driver()
+        self.error_urls = []
         
     def _get_driver(self):
         display = Display(visible=0, size=(1920, 1080))
@@ -35,22 +38,30 @@ class CrawlingDaumNews:
         driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
         return driver
 
-    def _driver_wait(self):
-        WebDriverWait(self.driver, 2).until(
-            expected_conditions.invisibility_of_element((By.CSS_SELECTOR, "__initial_loading"))
-        )
+    # def _driver_wait(self):
+    #     WebDriverWait(self.driver, 2).until(
+    #         expected_conditions.invisibility_of_element((By.CSS_SELECTOR, "__initial_loading"))
+    #     )
 
-    def generate_article_json(self, date, category):
+    def generate_article_json(self, date, category, start_page, page_count):
         title_data = self._get_title_data(date, category)
         
         json_result = []
         
-        for article in tqdm(title_data['articles'], total=len(title_data['articles'])):
+        articles = [article for article in title_data['articles'] if start_page <= int(article["id"].split("_")[0]) < start_page+page_count]
+
+        for article in tqdm(articles, total=len(articles)):
             info = self._get_article(category, article["url"])
-            json_result.append(info)
+            if info:
+                json_result.append(info)
             
-        with open(f"daum_articles_{date}_{category}.json", "w", encoding="utf-8") as f:
+        with open(f"daum_articles_{date}_{category}_{start_page:03d}.json", "w", encoding="utf-8") as f:
             json.dump(json_result, f, ensure_ascii=False)
+            
+        if self.error_urls:
+            print("fail url list")
+            for error_url in self.error_urls:
+                print(error_url)
 
     def _get_title_data(self, date, category):
         with open(f"daum_titles_{date}_{category}.json", "r", encoding="utf-8") as f:
@@ -61,21 +72,27 @@ class CrawlingDaumNews:
         info = dict()
         article_id = url.split('/')[-1]
 
+        self.driver.implicitly_wait(3)
         self.driver.get(url)
-        self._driver_wait()
+        #self._driver_wait()
 
         bsObject = BeautifulSoup(self.driver.page_source, "html.parser")
         
-        title = bsObject.select('#cSub .tit_view')[0].text
-        abstractive = bsObject.select('.summary_view')
-        abstractive = abstractive[0].get_text(strip=True, separator="\n").splitlines() if abstractive else []
-        article = [p.text for p in bsObject.select('#harmonyContainer > section p') if p.text != '']
-        article = self._corpus_to_sentence(article)
-        article = [{'index': idx, 'sentence': sentence} for idx, sentence in enumerate(article)]
-        date = bsObject.select('.info_view .num_date')[0].text
-        date_lst = date.replace('. ','-').split("-")
-        date = "-".join(date_lst[:-1]) + " " + date_lst[-1]
-
+        try:
+            title = bsObject.select('#cSub .tit_view')[0].text
+            abstractive = bsObject.select('.summary_view')
+            abstractive = abstractive[0].get_text(strip=True, separator="\n").splitlines() if abstractive else []
+            article = [p.text for p in bsObject.select('#harmonyContainer > section p') if p.text != '']
+            #article = self._corpus_to_sentence(article)
+            article = [{'index': idx, 'sentence': sentence} for idx, sentence in enumerate(article)]
+            date = bsObject.select('.info_view .num_date')[0].text
+            date_lst = date.replace('. ','-').split("-")
+            date = "-".join(date_lst[:-1]) + " " + date_lst[-1]
+        except Exception as e:
+            print(f"Parsing error ({url}): {e}")
+            self.error_urls.append(url)
+            return info
+        
         info['category'] = category
         info['id'] = article_id
         info['publish_date'] = date
@@ -96,12 +113,44 @@ class CrawlingDaumNews:
                     splited_article.append(new_sentence)
         return splited_article
 
+def get_args():
+    parser = argparse.ArgumentParser(description="crawling daum news titile.")
+    parser.add_argument(
+        "--date",
+        default=(date.today() - timedelta(1)).strftime("%Y%m%d"),
+        type=str,
+        help="date of news"
+    )
+    parser.add_argument(
+        "--category",
+        default="all",
+        type=str,
+        help="category of news",
+        choices=["all", "society", "politics", "economic", "foreign", "culture", "entertain", "sports", "digital"]
+    )
+    parser.add_argument(
+        "--start_page",
+        default=1,
+        type=int,
+        help="start page no",
+    )
+    parser.add_argument(
+        "--page_count",
+        default=10,
+        type=int,
+        help="page count",
+    )
+    args = parser.parse_args()
+    return args
+
 def main():
-    date = "20211205"
-    category = "IT"
-    
+    args = get_args()
     crawling_obj = CrawlingDaumNews()
-    crawling_obj.generate_article_json(date=date, category=category)
+    
+    date = args.date
+    category = {v:k for k, v in crawling_obj.categories.items()}[args.category]
+    
+    crawling_obj.generate_article_json(date=date, category=category, start_page=args.start_page, page_count=args.page_count)
 
 if __name__ == "__main__":
     main()
